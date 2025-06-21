d36c57c82c813831c4908f5dfb95160e5164da78
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-06-21 19:13:08 +0100

    com

--- accounts/views.py
+++ accounts/views.py
@@ -275,14 +275,7 @@ class GoogleSignInView(APIView):
         except ValueError:
             return Response({'error': 'Invalid Google token'}, status=status.HTTP_400_BAD_REQUEST)
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_decode, force_str
-from django.core.mail import send_mail
-from django.conf import settings
-import logging
+
 
 logger = logging.getLogger(__name__)
 
