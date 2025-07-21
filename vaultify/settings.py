b51cb2457642c120d11391f804f0b4e03b16ce73
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-07-22 00:25:43 +0100

    c

--- vaultify/settings.py
+++ vaultify/settings.py
@@ -46,23 +46,21 @@ INSTALLED_APPS = [
     'corsheaders',
     'accounts',
 ]
-
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Must be first
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    
 ]
 
-ALLOWED_HOSTS = ['vaultify.lexisdevelopmentltd.com', 'vaultify.lexisfinance.com', 'vaultify-43wm.onrender.com', '88.223.94.10', 'yourdomain.com', '127.0.0.1', ] 
+ALLOWED_HOSTS = ['vaultify.lexisdevelopmentltd.com', 'vaultify.lexisfinance.com', 'vaultify-43wm.onrender.com', 'vaultifyadmin.onrender.com', 'yourdomain.com', '127.0.0.1', ] 
 # or specify your IP
 
 CORS_ALLOWED_ORIGINS = ["https://vaultifyadmin.onrender.com"]
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
